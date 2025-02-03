7d5e57b99dbe0e9c394d9dfa66f5e65bf91b6424
Jose Morales Besalduch <pepemmbesalduch@gmail.com>
2025-02-03 14:16:04 +0100

    Initial commit with Vite, React and Tailwind CSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Para archivos en la carpeta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
